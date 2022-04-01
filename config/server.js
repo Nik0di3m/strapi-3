32453b715ffacd66c74d891cadcce902440f5f50
Nik0di3m <norbert.waleszczyk@icloud.com>
2022-04-01 10:52:07 +0200

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '7116f531fcbfeada8cb6475134f14c03'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '3978704d-a3a4-4fc1-a644-c70016eafe45'
+};
